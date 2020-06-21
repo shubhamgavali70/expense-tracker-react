f93b089ac1cd8536de105f7e4c5b3a42b54902c4
Shubham gavali <shubhamgavali70@gmail.com>
2020-06-21 21:09:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Header from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
+
+import "./App.css";
+import { Balance } from "./components/Balance";
+import TransactionList from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
+
+const App = () => {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+};
+export default App;
